48d5a4f0e5d29e3ee896065234f7a19c839d7c97
sergey <s.vandyshev@gmail.com>
2017-02-20 22:54:44 +0100

    better perspective transformation

--- scripts/roi_utils.py
+++ scripts/roi_utils.py
@@ -3,7 +3,7 @@ import cv2
 
 
 
-class RoiSelector(object):
+class Roi(object):
 
     @staticmethod
     def select_roi(img, vertices):
@@ -21,6 +21,18 @@ class RoiSelector(object):
         #returning the image only where mask pixels are nonzero
         masked_image = cv2.bitwise_and(img, mask)
         return masked_image
+
+
+    def transform(img, src_points, dst_points):
+        #import pdb
+        #pdb.set_trace()
+        src_points, dst_points = np.float32(src_points), np.float32(dst_points)
+        M = cv2.getPerspectiveTransform(src_points, dst_points)
+        warped = cv2.warpPerspective(img, M, img.shape[0:2][::-1])
+        return warped
+
+    def threshold(image):
+        return image
 
 
 
--- scripts/run.py
+++ scripts/run.py
@@ -1,6 +1,6 @@
 from calibrate_camera import CalibrationUtils
 from utils import Utils
-from roi_selector import RoiSelector
+from roi_utils import Roi
 from moviepy.editor import VideoFileClip
 import sys
 import numpy as np
@@ -13,19 +13,34 @@ print()
 mtx, dist = CalibrationUtils.load_calibration()
 clip = VideoFileClip(sys.argv[1])
 X,Y,Z = clip.w, clip.h, 3
-roishape = np.array([[[0, Y-10], [int(X/2), Y-int(Y/2.5)], [X,Y-10]]], dtype=np.int32)
+
+roishape = np.array([[[0, Y-10], 
+                      [int(X/2)-100, Y-int(Y/2.5)], 
+                      [int(X/2)+100, Y-int(Y/2.5)], 
+                      [X,Y-10]]], 
+                      dtype=np.int32)
+
+roi_dst_shape = np.array([[[int(X/4), Y-10], 
+                           [int(X/2)-400, 0], 
+                           [int(X/2)+400, 0], 
+                           [X-int(X/4),Y-10]]], 
+                           dtype=np.int32)
 
 def process_image(image):
-    #undistort
+    # undistort
     undistorted_image = CalibrationUtils.undistort_img(image, mtx, dist)
-    Utils.compare_before_after(image, undistorted_image)
-    # TODO - select region
-    roi = RoiSelector.select_roi(undistorted_image, roishape)
-    Utils.compare_before_after(image, roi)
 
-    # TODO - wrap??
+    # select region
+    roi = Roi.select_roi(undistorted_image, roishape)
+    #Utils.compare_before_after(image, roi)
+
+    # transform
+    transformed_roi = Roi.transform(roi, roishape.reshape(4,2), roi_dst_shape)
+    Utils.compare_before_after(roi, transformed_roi)
     
-    # TODO - edge detection
+    # TODO - color gradient threshold
+    threshold = Roi.threshold(transformed_roi)
+    Utils.compare_before_after(roi, threshold)
 
     # TODO - detect lane
 
